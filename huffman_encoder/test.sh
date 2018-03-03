c077df8eee0776828fe5719e7779556713a835e8
Max <alvinmax@mail.ru>
2018-03-03 14:42:52 +0300

    Update test.sh

--- huffman_encoder/test.sh
+++ huffman_encoder/test.sh
@@ -1,6 +1,7 @@
 cd build
 cd utility
-touch test2.jpg
 echo "Compressing png"
 ./huffman -c /home/max/Рабочий\ стол/huffman_encoder/test.jpg /home/max/Рабочий\ стол/huffman_encoder/dst.huf
+touch test2.jpg
+echo "Decompressing png"
 ./huffman -d /home/max/Рабочий\ стол/huffman_encoder/dst.huf /home/max/Рабочий\ стол/huffman_encoder/test2.jpg
