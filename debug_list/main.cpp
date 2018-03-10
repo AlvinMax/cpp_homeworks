29105c195f03539271cf4c0c0dc1a469c6e36af1
Max <alvinmax@mail.ru>
2018-03-10 20:35:58 +0300

    Add files via upload

---
+++ debug_list/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "debug_list.h"
+#include <vector>
+#include <list>
+
+using namespace std;
+
+
+int main() {
+
+    debug_list<int> S;
+    S.push_back(5);
+    return 0;
+}
